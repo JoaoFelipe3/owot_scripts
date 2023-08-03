44bfbbc9995751093506ce94132c6b236d9c832d
JoaoFelipe3 <109555243+JoaoFelipe3@users.noreply.github.com>
2023-08-03 15:17:27 -0300

    COOLDOWN

--- mombot.js
+++ mombot.js
@@ -1,3 +1,8 @@
 api_chat_send("MOMBOT", {nick: "mombot"})
 
-w.on("chatmod", (e) => e.message.startsWith("!mom") ? api_chat_send("yo mama so " + e.message.slice(4), {nick: "mombot"}) : null)
+const syncSleep = ms => {
+  const end = new Date.getTime() + ms;
+  while (new Date.getTime() < end);
+}
+
+w.on("chatmod", (e) => e.message.startsWith("!mom") ? api_chat_send("yo mama so " + e.message.slice(4), {nick: "mombot"}) || syncSleep(2) : null)
