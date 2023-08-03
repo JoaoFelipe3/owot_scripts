d1dac4ae1b7dd60b36892397007c8899167a7217
JoaoFelipe3 <109555243+JoaoFelipe3@users.noreply.github.com>
2023-08-03 14:58:36 -0300

    BUGFIX

--- mombot.js
+++ mombot.js
@@ -1,3 +1,3 @@
 api_chat_send("MOMBOT", {nick: "mombot"})
 
-w.on("chatmod", (e) => e.message.startsWith("!mom") ? api_chat_send("yo mama so " + e.message.slice(4)) : null)
+w.on("chatmod", (e) => e.message.startsWith("!mom") ? api_chat_send("yo mama so " + e.message.slice(4), {nick: "mombot"}) : null)
